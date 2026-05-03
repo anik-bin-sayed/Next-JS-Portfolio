5063e92c4828a34828dc54e06105554de3fdc676
Anik_Bin_Sayed <anikhossen3422@gmail.com>
2026-05-03 12:09:57 +0600

    add sitemap

---
+++ src/app/sitemap.js
@@ -0,0 +1,24 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://anik-bin-sayed.vercel.app/",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://anik-bin-sayed.vercel.app/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://anik-bin-sayed.vercel.app/service",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://anik-bin-sayed.vercel.app/portfolio",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://anik-bin-sayed.vercel.app/contact",
+      lastModified: new Date(),
+    },
+  ];
+}
